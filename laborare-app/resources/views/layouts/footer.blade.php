8da6b64ae8a34470380364026403473411d757ac
Nikitags180 <nikitae372@gmail.com>
2024-04-03 22:54:04 +0700

    K3RPL-9 progress beranda

--- laborare-app/resources/views/layouts/footer.blade.php
+++ laborare-app/resources/views/layouts/footer.blade.php
@@ -1,6 +1,6 @@
 <section  style="color:white;">
     <!-- Footer -->
-    <footer class="bg-black position-absolute bottom-0" style="margin-left: 8%">
+    <footer class="bg-black">
       <!-- Grid container -->
       <div class="container p-4" >
         <!--Grid row-->
@@ -8,7 +8,7 @@
           <!--Grid column-->
           <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
             <h5 class="text-uppercase">Nama</h5>
-  
+
             <ul class="list-unstyled mb-0">
               <li>
                 <p>Inggit Yeira Budi Agranata</p>
@@ -31,11 +31,11 @@
             </ul>
           </div>
           <!--Grid column-->
-  
+
           <!--Grid column-->
           <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
             <h5 class="text-uppercase" style="text-align: center;">NIM</h5>
-  
+
             <ul class="list-unstyled mb-0" style="text-align: center;">
               <li>
                 <p>1202213238</p>
@@ -58,11 +58,11 @@
             </ul>
           </div>
           <!--Grid column-->
-  
+
           <!--Grid column-->
           <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
             <h5 class="text-uppercase">Universitas</h5>
-  
+
             <ul class="list-unstyled mb-0">
               <li>
                 <p>Telkom University</p>
@@ -70,11 +70,11 @@
             </ul>
           </div>
           <!--Grid column-->
-  
+
           <!--Grid column-->
           <div class="col-lg-5 col-md-12 mb-4 mb-md-0" style="background: white; color:black;">
             <h5 class="text-uppercase" style="font-size: 80px;"><strong>Laborare</strong></h5>
-  
+
             <p><strong>PILIHAN TERBAIK UNTUK MENJADI SUKARELAWAN</strong></p>
             <br>
             <p>Bergabunglah bersama kami dan buka dunia manfaat eksklusif. Jadilah yang pertama mengetahui kegiatan baru kami untuk sukarelawan, serta pembaruan yang seru.</p>
@@ -84,7 +84,7 @@
         <!--Grid row-->
       </div>
       <!-- Grid container -->
-  
+
       <!-- Copyright -->
       <div class="text-center p-3" style="background-color: black; color:white;">
           © SI4502 - REKAYASA PERANGKAT LUNAK - KELOMPOK 3
@@ -92,4 +92,4 @@
       <!-- Copyright -->
     </footer>
     <!-- Footer -->
-  </section>
+  </section>
