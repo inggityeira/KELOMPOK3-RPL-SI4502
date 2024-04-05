4fe59af470dd886977520b817282d2e7688d8950
luckyahh <lhandojo6@gmail.com>
2024-04-06 01:04:22 +0700

    K3RPL-15 commit profil individu done

--- laborare-app/resources/views/profil/profilindividu.blade.php
+++ laborare-app/resources/views/profil/profilindividu.blade.php
@@ -8,35 +8,55 @@
 
 @section('content')
 {{-- ISI KONTEN KALIAN DIBAWAH INI --}}
-        <div class="mt-4">
+<div class="container">
+   <a href="/profil-Ind" style="text-decoration: none; color: white;">Profile</a>
+</div>
 
-    <div class="container d-flex justify-content-center">
-        <form action="" class="col-md-6">
-            <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email">
-            </div>
-            <div class="form-group mt-2">
-                <label for="no">Nomer Telepon</label>
-                <input type="tel" class="form-control" id="no">
-            </div>
-            <div class="form-group row mt-2">
-                <div class="col">
-                    <label for="Usia">Usia</label>
-                    <input type="number" class="form-control" id="Usia">
-                </div>
-            <div class="form-group mt-4">
-                <button type="submit" class="btn btn-light mr-2">Edit</button>
-                <a href="/logout" class="btn btn-danger">Keluar</a>
-            </div>
-        </form>
+<div class="d-flex flex-column align-items-center mt-5 text-center">
+    <img src="/fotoprofil/inggit.jpg" alt="..." class="rounded-circle" width="130px" height="130px">
+    <div class="mt-4">
+        <h2 class="fw-bolder">INGGIT YEIRA BUDI A.</h2>
+        <p>Jl. Telekomunikasi. 1, Terusan Buahbatu - Bojongsoang, Telkom University, Sukapura, Kec. Dayeuhkolot, <br> Kabupaten Bandung, Jawa Barat 40257</p>
     </div>
+</div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+<div class="container d-flex justify-content-center">
+    <form action="" class="col-md-6">
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" id="email">
+        </div>
+        <div class="form-group mt-2">
+            <label for="no">Nomer Telepon</label>
+            <input type="tel" class="form-control" id="no">
+        </div>
+        <div class="form-group row mt-2">
+            <div class="col">
+                <label for="Usia">Usia</label>
+                <input type="number" class="form-control" id="Usia">
+            </div>
+            <div class="col mt-2">
+                <label>Jenis Kelamin</label>
+                <div>
+                    <input type="radio" id="Pria" name="jenis_kelamin" value="Pria">
+                    <label for="Pria">Pria</label>
+                    <input type="radio" id="Wanita" name="jenis_kelamin" value="Wanita">
+                    <label for="Wanita">Wanita</label>
+                </div>
+            </div>
+        </div>
+        <div class="form-group mt-4">
+            <a href="/editprofil-Ind" type="submit" class="btn btn-light mr-2">Edit</a>
+            <a href="" type="submit" class="btn btn-danger">Keluar</a>
+        </div>
+    </form>
+</div>
 
-</body>
-</html>
 
+
+
+
+
+
+    
 @endsection
