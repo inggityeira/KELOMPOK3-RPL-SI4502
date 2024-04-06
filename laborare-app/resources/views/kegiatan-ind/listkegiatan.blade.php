28b6670b1f7a3635a4aa7379fef66b31905fbc93
Nikitags180 <nikitae372@gmail.com>
2024-04-06 22:11:56 +0700

    K3RPL-11 views list kegiatan

--- laborare-app/resources/views/kegiatan-ind/listkegiatan.blade.php
+++ laborare-app/resources/views/kegiatan-ind/listkegiatan.blade.php
@@ -19,13 +19,13 @@
         <h4>Kegiatan Yang Akan Datang</h4>
       </div>
       <div class="col">
-        <a href="" class="btn btn-light">Kegiatan yang diikuti</a>
+        <a href="" class="btn btn-light fw-bolder">Kegiatan yang diikuti</a>
       </div>
     </div>
 </div>
 
 <div class="kegiatan-home">
-    <img src="img/listKegiatan/home.png" class="img-fluid" alt="">
+    <img src="img/listKegiatan/kegiataan.jpg" class="img-fluid" alt="">
     <div class="text-kegiatan-home">
       <h5 class="fw-bolder">Kesempatan untuk membuat perbedaan ada di tangan Anda</h5>
     </div>
@@ -36,21 +36,21 @@
   <div class="container d-flex justify-content-center">
     <div class="row">
       <div class="col position-relative">
-        <img src="img/listKegiatan/f.jpg" alt="" width="300px" height="250px">
+        <img src="img/listKegiatan/1.1.jpg" alt="" width="300px" height="250px">
         <div class="fill-overlay"></div>
         <div class="card-body position-absolute bottom-0 start-50 translate-middle-x z-index-1">
           <p class="card-text fw-bolder text-center text-white">Gendong Untuk Kesetaraan</p>
         </div>
       </div>
       <div class="col position-relative">
-        <img src="img/listKegiatan/a.jpg" alt="" width="300px" height="250px">
+        <img src="img/listKegiatan/1.2.jpg" alt="" width="300px" height="250px">
         <div class="fill-overlay"></div>
         <div class="card-body position-absolute bottom-0 start-50 translate-middle-x z-index-1">
           <p class="card-text fw-bolder text-center text-white">Sukarelawan Sinar Kebaikan</p>
         </div>
       </div>
       <div class="col position-relative">
-        <img src="img/listKegiatan/d.jpg" alt="" width="300px" height="250px">
+        <img src="img/listKegiatan/1.3.jpg" alt="" width="300px" height="250px">
         <div class="fill-overlay"></div>
         <div class="card-body position-absolute bottom-0 start-50 translate-middle-x z-index-1">
           <p class="card-text fw-bolder text-center text-white">Hati Yang Rela Berbagi</p>
@@ -60,5 +60,34 @@
   </div>
 </div>
 
+<div class="">
+    <div class="container d-flex justify-content-center">
+      <div class="row">
+        <div class="col position-relative">
+          <img src="img/listKegiatan/1.4.jpg" alt="" width="300px" height="250px">
+          <div class="fill-overlay"></div>
+          <div class="card-body position-absolute bottom-0 start-50 translate-middle-x z-index-1">
+            <p class="card-text fw-bolder text-center text-white">Beri Waktu Raih Perubahan</p>
+          </div>
+        </div>
+        <div class="col position-relative">
+          <img src="img/listKegiatan/1.5.jpg" alt="" width="300px" height="250px">
+          <div class="fill-overlay"></div>
+          <div class="card-body position-absolute bottom-0 start-50 translate-middle-x z-index-1">
+            <p class="card-text fw-bolder text-center text-white">Cinta Dalam Aksi Sinar</p>
+          </div>
+        </div>
+        <div class="col position-relative">
+          <img src="img/listKegiatan/1.6.jpg" alt="" width="300px" height="250px">
+          <div class="fill-overlay"></div>
+          <div class="card-body position-absolute bottom-0 start-50 translate-middle-x z-index-1">
+            <p class="card-text fw-bolder text-center text-white">Menebar Kebaikan Meraih Bahagia</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<br>
 
 @endsection
