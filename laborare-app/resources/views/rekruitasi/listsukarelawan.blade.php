423e5a79da90f928a64e82d09d265f43081c41fa
DRICK-PC\Hendrickus <hendrickusbalzano@gmail.com>
2024-05-01 23:35:15 +0700

    K3RPL-20 Membuat list sukarelawan untuk organisasi

--- laborare-app/resources/views/rekruitasi/listsukarelawan.blade.php
+++ laborare-app/resources/views/rekruitasi/listsukarelawan.blade.php
@@ -9,5 +9,24 @@
 @section('content')
 {{-- ISI KONTEN KALIAN DIBAWAH INI --}}
 LIST SUKARELAWAN
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Genius Mode</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    
 
 @endsection
