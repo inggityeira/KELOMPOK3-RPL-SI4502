80ab29eca73ab332a2c7caee7992129df8e3b55a
Nikitags180 <nikitae372@gmail.com>
2024-05-01 23:26:08 +0700

    K3RPL-22 progress

--- laborare-app/resources/views/rekruitasi/rekrut.blade.php
+++ laborare-app/resources/views/rekruitasi/rekrut.blade.php
@@ -7,7 +7,27 @@
 @endpush
 
 @section('content')
-{{-- ISI KONTEN KALIAN DIBAWAH INI --}}
-REKRUT
+<div class="d-flex flex-column align-items-center  text-center" >
+    {{-- Foto --}}
+    @if($user->foto_profil != null)
+            <img src="{{asset('fotoprofil/'.$user->foto_profil)}}" alt="Profile" style="border-radius: 50%; width: 150px; height: 150px;">
+        @else
+            <img src="/kosong.png" alt="Profile" style="border-radius: 50%; width: 150px; height: 150px;">
+    @endif
+    <div class="container d-flex justify-content-center">
+        <form style="width: 55%">
+            <div class="mb-2">
+                <label for="nama_sukarewan">Nama</label>
+                <div class="card p-2">{{ $user ->id_user }}</div>
+            </div>
+            <div class="mb-2">
+                <label for="status">Kegiatan</label>
+                <div class="card p-2">{{ $user ->id_kegiatan }}</div>
+            </div>
+            <div class="form-group row mt-2">
+                <div class="col">
+                    <label for="Status">Status Penerimaan</label>
+                    <div class="card p-2">{{ $user ->status_sukarelawan}}</div>
+                </div>
 
 @endsection
