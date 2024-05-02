f04ebdf08ca8395db343a339b8aaca1e9a136970
Nikitags180 <nikitae372@gmail.com>
2024-05-03 00:01:57 +0700

    K3RPL-22 front endd

--- laborare-app/resources/views/rekruitasi/rekrut.blade.php
+++ laborare-app/resources/views/rekruitasi/rekrut.blade.php
@@ -7,27 +7,78 @@
 @endpush
 
 @section('content')
+{{-- ISI KONTEN KALIAN DIBAWAH INI --}}
+@php
+    $user = \App\Models\User::where('id_user', session('loginId'))->first();
+@endphp
+
+{{-- Breadscrumb --}}
+<div style="margin-top:30px; margin-left:50px;">
+    <style>
+      .breadcrumb-item+.breadcrumb-item::before {
+        color: white;
+        font-size: 20px;
+      }
+    </style>
+    <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="/listsukarelawan" style="color:white; font-size:20px; text-decoration: underline; letter-spacing:1px; font-size:18px;"><strong>Rekruitasi</strong></a></li>
+            <li class="breadcrumb-item"><a href="" style="color:white; font-size:20px; text-decoration: underline; letter-spacing:1px; font-size:18px;"><strong>Rekrut Sukarelawan</strong></a></li>
+        </ol>
+    </nav>
+</div>
+
 <div class="d-flex flex-column align-items-center  text-center" >
-    {{-- Foto --}}
+    {{-- Foto Profil --}}
     @if($user->foto_profil != null)
-            <img src="{{asset('fotoprofil/'.$user->foto_profil)}}" alt="Profile" style="border-radius: 50%; width: 150px; height: 150px;">
+            <img src="{{asset('fotoprofil/'.$user->foto_profil)}}" alt="Profile" style="border-radius: 50%; width: 250px; height: 250px;">
         @else
-            <img src="/kosong.png" alt="Profile" style="border-radius: 50%; width: 150px; height: 150px;">
+            <img src="{{asset('fotoprofil/kosong.png')}}" alt="Profile" style="border-radius: 50%; width: 250px; height: 250px;">
     @endif
-    <div class="container d-flex justify-content-center">
-        <form style="width: 55%">
-            <div class="mb-2">
-                <label for="nama_sukarewan">Nama</label>
-                <div class="card p-2">{{ $user ->id_user }}</div>
+
+    {{-- Nama dan Alamat --}}
+
+</div>
+
+{{-- Detail Profil --}}
+<div class="container d-flex justify-content-center">
+    <form style="width: 55%">
+        <div class="mb-2">
+            <label for="nama">Nama Lengkap</label>
+            <div class=""><input type="text" class="card p-2" style="width: 100%; padding: 8px;"></div>
+        </div>
+        <div class="mb-2">
+            <label for="kegiatan">Kegiatan</label>
+            <div class="">
+                <select class="card p-2" style="width: 100%; padding: 8px;">
+                    <option value="meeting">Meeting</option>
+                    <option value="presentasi">Presentasi</option>
+                    <option value="training">Training</option>
+                    <option value="lainnya">Lainnya</option>
+                </select>
             </div>
-            <div class="mb-2">
-                <label for="status">Kegiatan</label>
-                <div class="card p-2">{{ $user ->id_kegiatan }}</div>
+        </div>
+        <div class="mb-2">
+            <label for="status">Status Penerimaan</label>
+            <div class="">
+                <select class="card p-2" style="width: 100%; padding: 8px;">
+                    <option value="diterima">Diterima</option>
+                    <option value="ditolak">Ditolak</option>
+                    <option value="menunggu">Menunggu</option>
+                </select>
             </div>
-            <div class="form-group row mt-2">
-                <div class="col">
-                    <label for="Status">Status Penerimaan</label>
-                    <div class="card p-2">{{ $user ->status_sukarelawan}}</div>
-                </div>
+        </div>
+        <div class="form-group center-button mt-4">
+                <a href="/rekrutsukarelawan" class="btn btn-outline-light px-4"><strong>Rekrut</strong></a>
+            </div>
+    </form>
+</div>
+
+
+
+
+
+
+
 
 @endsection
