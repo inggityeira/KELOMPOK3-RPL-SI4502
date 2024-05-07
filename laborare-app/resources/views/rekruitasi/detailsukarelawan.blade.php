b3483477ff1812773908ec85bce2c66867e5de0e
luckyahh <lhandojo6@gmail.com>
2024-05-07 23:02:43 +0700

    K3RPL-21 detailsukarelawan

--- laborare-app/resources/views/rekruitasi/detailsukarelawan.blade.php
+++ laborare-app/resources/views/rekruitasi/detailsukarelawan.blade.php
@@ -13,33 +13,32 @@
         <div class="col-md-6 pt-4">
             <img src="{{asset('pasfoto/'.$sukarelawan->pas_foto)}}" alt="Gambar" class="object-fill rounded-full">
         </div>
-        
         <div class="col-md-6" style="margin-left: -150px;">
             <form action="" class="col-md-12">
                 <div class="form-group">
                     <label for="nama">Nama Lengkap</label>
-                    <input type="nama" class="form-control" id="nama" value="{{$sukarelawan->id_sukarelawan}}">
+                    <div class="form-control" id="nama">{{ $sukarelawan->user->nama_user }}</div>
                 </div>
                 <div class="form-group mt-2">
                     <label for="Nomor Telepon">Nomor Telepon</label>
-                    <input type="tel" class="form-control" id="no" value="{{$sukarelawan->notelpon_sukarelawan}}">
+                    <div class="form-control" id="no">{{ $sukarelawan->notelpon_sukarelawan }}</div>
                 </div>
                 <div class="form-group mt-2">
-                    <label for="Kontak _Wali">Kontak Wali</label>
-                    <input type="kontak" class="form-control" id="kontak" value="{{$sukarelawan->kontak_wali}}">
+                    <label for="Kontak_Wali">Kontak Wali</label>
+                    <div class="form-control" id="kontak">{{ $sukarelawan->kontak_wali }}</div>
                 </div>
                 <div class="form-group mt-2">
                     <label for="Alamat">Alamat</label>
-                    <textarea rows="5" cols="77">{{$sukarelawan->alamat_sukarelawan}}</textarea>
+                    <textarea rows="5" cols="77" readonly>{{ $sukarelawan->alamat_sukarelawan }}</textarea>
                 </div>
                 <div class="form-group mt-2">
                     <label for="Motivasi">Motivasi Mengikuti Kegiatan</label>
-                    <textarea rows="5" cols="77">{{$sukarelawan->motivasi}}</textarea>
+                    <textarea rows="5" cols="77" readonly>{{ $sukarelawan->motivasi }}</textarea>
                 </div>
                 <div class="form-group center-button mt-4" style="display: flex; justify-content: end;">
-                    <a href="{{ route('rekrut', ['id' => $sukarelawan->id_sukarelawan]) }}" type="submit" class="btn px-4" style="background-color: white; color: black;"><strong>Rekrut</strong></a>
+                    <a href="{{ route('rekrut-sukarelawan', ['id' => $sukarelawan->id_sukarelawan]) }}" class="btn px-4" style="background-color: white; color: black;"><strong>Rekrut</strong></a>
                 </div>
-            </form>
+            </form>                
         </div>
     </div>
 </div>
