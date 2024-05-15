7b06118fb75b1c5aec245b90bd897029f97156b5
shobarna <shobarnai@gmail.com>
2024-05-15 11:27:14 +0700

    Start Progress PBI23

---
+++ laborare-app/resources/views/poin/tablepoint.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.main')
+
+@section('title', 'LABORARE | POIN')
+
+@push('css')
+    {{-- ISI CSS KALIAN, BISA EKSTERNAL/INTERNAL --}}
+@endpush
+
+@section('content')
+    {{-- ISI KONTEN KALIAN DIBAWAH INI --}}
+    JUMLAH POIN
+
+@endsection
