f366c52f3b6b644afb21b7beb59f9a609999342a
shobarna <shobarnai@gmail.com>
2024-05-18 03:32:30 +0700

    PBI23

--- laborare-app/resources/views/poin/tablepoint.blade.php
+++ laborare-app/resources/views/poin/tablepoint.blade.php
@@ -8,6 +8,44 @@
 
 @section('content')
     {{-- ISI KONTEN KALIAN DIBAWAH INI --}}
-    JUMLAH POIN
+    <div style="margin-top:30px; margin-left:50px;">
+        <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item active"><a href="/jumlahpoin"
+                        style="color:white; font-size:20px; text-decoration: none; letter-spacing:1px;">Poin</a></li>
+                <li class="breadcrumb-item active"><a href="{{ url()->current() }}"
+                        style="color:white; font-size:20px; text-decoration: underline; letter-spacing:1px;">Riwayat Poin</a>
+                </li>
+            </ol>
+        </nav>
+    </div>
+
+    <div class="container">
+        <center>
+            <h3 class="fs-3 fw-semibold">RIWAYAT POINT</h3>
+            <div class="card mx-4" style="max-width: 600px">
+                <div class="card-body">
+                    <table class="table table-borderless rounded-4">
+                        <thead class="table-secondary">
+                            <tr class="text-center" style="font-size: 14px;">
+                                <th class="text-secondary">NO</th>
+                                <th class="text-secondary">NAMA KEGIATAN</th>
+                                <th class="text-secondary">JUMLAH POIN</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($data as $index => $item)
+                                <tr class="text-center">
+                                    <td>{{ $index + 1 }}</td>
+                                    <td>{{ $item->kegiatan->nama_kegiatan }}</td>
+                                    <td>{{ $item->poin }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </center>
+    </div>
 
 @endsection
