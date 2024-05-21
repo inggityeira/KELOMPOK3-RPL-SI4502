c97e786a63b250116886c6bb9dc78098ca6cacdd
inggityeira <inggitagranata22@gmail.com>
2024-05-21 19:56:22 +0700

    K3RPL-23 New Donasi Done progres detail Donasi

--- laborare-app/resources/views/donasi-org/donasibaru.blade.php
+++ laborare-app/resources/views/donasi-org/donasibaru.blade.php
@@ -5,10 +5,30 @@
 @push('css')
     {{-- ISI CSS KALIAN, BISA EKSTERNAL/INTERNAL --}}
     <style>
+        body {
+            color: black;
+        }
+
         .judul {
             font-size: 25px;
-            margin-top: 40px;
-            margin-left: 130px;
+            color: white;
+            padding: 5px;
+            margin-bottom: 30px;
+        }
+
+        .formNewDonasi {
+            width: 70%;
+            margin: 0 auto;
+            background-color: white;
+            padding: 20px;
+            border-radius: 15px;
+        }
+
+        button {
+            color: white;
+            padding: 10px 20px;
+            border: none;
+            border-radius: 5px;
         }
     </style>
 @endpush
@@ -30,7 +50,45 @@
         </nav>
     </div>
 
-    
+    {{-- Judul --}}
+    <div class="d-flex justify-content-center">
+        <h1 class="judul"><strong>TAMBAH DONASI</strong></h1>
+    </div>
+
+    {{-- Form Donasi Baru --}}
+    <div class="formNewDonasi">
+        <form action="{{ route('tambahdonasi') }}" method="POST" enctype="multipart/form-data">
+            @if (session('success'))
+                <div class="alert alert-success">
+                    {{ session('success') }}
+                </div>
+            @endif
+
+            @csrf
+            
+            <div class="mb-3">
+                <label class="form-label">Nama Donasi</label>
+                <input type="text" name="nama_donasi" class="form-control" style="background-color: #CBCBCB;">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Target Donasi</label>
+                <input type="number" name="target_donasi" class="form-control" style="background-color: #CBCBCB;">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Sampul Donasi</label>
+                <input type="file" name="sampul_donasi" class="form-control" style="background-color: #CBCBCB;">
+            </div>
+            <div class="mb-3">
+                <label class="form-label">Deskripsi Donasi</label>
+                <input type="text" name="deskripsi_donasi" class="form-control"
+                    style="height: 150px; background-color: #CBCBCB;">
+            </div>
+            <button type="submit"
+                style="float: right; background-color:black; color:white; width:150px;"><strong>Tambah</strong></button>
+        </form>
+    </div>
+
+
 
 
 
