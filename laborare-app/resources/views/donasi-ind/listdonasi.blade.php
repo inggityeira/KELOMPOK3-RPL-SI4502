665ab69ef3ec7493783b6f6b62e86fc0d48475c4
inggityeira <inggitagranata22@gmail.com>
2024-05-25 20:04:22 +0700

    K3RPL-24 Perbaiki List Donasi Individu

--- laborare-app/resources/views/donasi-ind/listdonasi.blade.php
+++ laborare-app/resources/views/donasi-ind/listdonasi.blade.php
@@ -4,10 +4,35 @@
 
 @push('css')
     {{-- ISI CSS KALIAN, BISA EKSTERNAL/INTERNAL --}}
+    <style>
+        .donasi-banner {
+            height: 300px;
+        }
+
+        .donasi-banner img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+            object-position: center;
+        }
+
+        .judul {
+            font-size: 25px;
+            margin-top: 40px;
+            margin-left: 130px;
+        }
+        .pagination-container .pagination .page-link {
+            color: white;
+            background-color: black;
+            border-color: white;
+        }
+    </style>
 @endpush
 
 @section('content')
     {{-- ISI KONTEN KALIAN DIBAWAH INI --}}
+    
+    {{-- Breadscrumb --}}
     <div style="margin-top:20px; margin-left:50px;">
         <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
             <ol class="breadcrumb">
@@ -18,15 +43,24 @@
         </nav>
     </div>
 
-    <div class="container-fluid p-0">
-        <img src="{{ asset('img/hero-listdonasiind.jpeg') }}" alt="hero-background" class="img-fluid p-0">
-        <h3 class="p-4 fs-4 fw-semibold">Bergabunglah Dalam Meberi</h3>
+    {{-- Banner --}}
+<div class="donasi-banner" style="position: relative; text-align: center;">
+    <h2 style="position: absolute; top: 100px; left: 20%; color: white;  padding: 10px;">
+        <strong>Tindakan Kecil, Dampak Besar: Donasikan Sekarang!</strong>
+    </h2>
+    <img src="{{ url('/img/DonasiInd.jpg') }}" class="img-fluid" alt="donasi">
+</div>
+
+
+    {{-- Judul --}}
+    <div>
+        <h1 class="judul"><strong>Bergabunglah Dalam Memberi,</strong></h1>
     </div>
 
     <div class="container px-5 text-center">
         <center class="px-4">
             <div class="row justify-content-center align-items-center">
-                @foreach ($data as $item)
+                @foreach ($donasi as $item)
                     <div class="col-4">
                         <div class="card rounded-0 mb-3" style="width: 24rem;">
                             <img src="{{ asset('donasi/' . $item->sampul_donasi) }}" class="card-img-top object-fit-cover"
@@ -35,9 +69,10 @@
                                 <h5 class="card-title text-start fw-semibold">{{ $item->nama_donasi }}</h5>
                                 <div class="d-flex align-items-center justify-content-center">
                                     <div class="container rounded-pill py-2 my-auto me-4" style="background-color: #e8e8e8">
-                                        <p class="m-0">Donasi : @currency($item->donatur->sum('nominal')) / @currency($item->target_donasi)</p>
+                                        <p class="m-0">Donasi : {{ $totalNominals[$item->id_donasi] ?? 'Rp 0' }} /
+                                            {{ $item->target_donasi }}</p>
                                     </div>
-                                    <a href="{{ route('detaildonasi', ['id' => $item->id_donasi]) }}"
+                                    <a href="{{ route('detail-donasi', ['id' => $item->id_donasi]) }}"
                                         class="btn btn-dark rounded-pill px-4 py-2">
                                         <span style="font-size: 10px">DETAIL</span>
                                     </a>
@@ -47,6 +82,11 @@
                     </div>
                 @endforeach
             </div>
+            {{-- Pagination --}}
+            <div class="pagination-container d-flex justify-content-center">
+                {{ $donasi->links('pagination::bootstrap-4') }}
+            </div>
         </center>
-    </div>
+        
+    </div>
 @endsection
